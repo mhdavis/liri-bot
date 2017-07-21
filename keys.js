96eaca872bb69138867d1cc54f9b7c3d2142bc7c
Michael Davis <mhdavis1993@gmail.com>
2017-07-21 18:29:44 -0400

    Began writing liri.js logic
    - Added keys.js with twitter api keys
    - Added random.txt for spotify npm usage
    - Wrote out commands for liri.js

--- keys.js
+++ keys.js
@@ -1,1 +1,8 @@
+console.log('this is loaded');
 
+exports.twitterKeys = {
+  consumer_key: 'T3Pu6GA2OcVvF2aVccF4bP0Cz',
+  consumer_secret: 'd9asmP8RmzF7nbb3HMgKdMyyOl4AWNMZvcEqXTZcbsvSF9Nvb8',
+  access_token_key: '887345485547274240-mIXHwqcvxjzdNLcsYi5lztCAnlvu4aw',
+  access_token_secret: 'guMJ2FoiDNWfkhMceEGzZ60MwouzKgdUowsybxixUxc59',
+}
---
+++ liri.js
@@ -0,0 +1,16 @@
+const keys =  require("keys.js");
+
+let input  = process.argv[2].toLowerCase();
+
+if (input === "my-tweets") {
+
+}
+else if (input === "spotify-this-song") {
+
+}
+else if (input === "movie-this") {
+
+}
+else if (input === "do-what-it-says") {
+
+}
