a61d47f882eca6f2fe2fccb6c7e9ef5910526194
Michael Davis <mhdavis1993@gmail.com>
2017-07-21 19:33:58 -0400

    Continued building out liri.js logic
    - Filled in twitter functionality; need to debug and get appropriate
      parameters in
    - Filled in spotify functionality; need to debug and get approrpiate
      parameters in
    - Created additional variables for keys and params for various npms

--- liri.js
+++ liri.js
@@ -1,11 +1,44 @@
-const keys =  require("keys.js");
+// Node Packages
+const Twitter = require('twitter');
+const Request = require('request');
+const Spotify = require('node-spotify-api');
 
+// Keys and Params
+const twitterKeys =  require("./keys.js");
+const twitterParams = {screen_name: 'TeamSoloMid'};
+const spotifyParams = {
+  id: false,
+  secret: false
+};
+
+// Inputs
 let input  = process.argv[2].toLowerCase();
 
-if (input === "my-tweets") {
+// Instances
+let client = new Twitter(twitterKeys);
+// let spotify = new Spotify(spotifyParams);
 
+if (input === "my-tweets") {
+  client.get('statues/user-timeline', twitterParams, function (error, tweets, response) {
+    if (!error) {
+      throw error;
+    }
+    console.log("\n------TWEETS-----\n");
+    console.log(tweets);
+    // console.log("\n------RAW TWITTER RESPONSE-----\n");
+    // console.log(response);
+
+  });
 }
 else if (input === "spotify-this-song") {
+  let song = process.argv[3]
+  spotify.search({type: 'track', query: song}, function (error, data) {
+    if (error) {
+      return console.log('Error occupied: ' + error);
+    }
+
+    console.log(data);
+  });
 
 }
 else if (input === "movie-this") {
